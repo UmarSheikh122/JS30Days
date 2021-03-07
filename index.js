d22570cb7e91e85b80cb293b13606a301949ece9
Umar Farooq (Trapezegroup Europe) <umar.farooq@trapezegroup.com>
2021-03-07 22:04:50 +0500

    JS first Day challenge

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener('keydown', (e)=>{
+    const audio =document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    key.classList.toggle('playing');
+    if(!audio) return
+    audio.currentTime = 0;
+    audio.play()
+})
